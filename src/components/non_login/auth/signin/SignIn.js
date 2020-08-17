34bbf8e24d5a64432d8507d4468351bd49a8839e
devxyoon <o_ov0709@naver.com>
2020-08-17 20:39:25 +0900

    20200817 08:38 Yoonjung

--- src/components/non_login/auth/signin/SignIn.js
+++ src/components/non_login/auth/signin/SignIn.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import kakao from "../../../../assets/signIn/kakao.png";
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useHistory } from "react-router-dom";
 import {
   SignInContainer,
   LoginContainer,
@@ -8,52 +8,98 @@ import {
   InputGroup,
   Row,
   Label,
-  WarnId,
   InputBorder,
   LoginInput,
-  WarnPassword,
   LoginLinkContainer,
   OriginLoginBtn,
+  Buttons,
+  ImgBtnContainer,
+  OtherLoginBtn,
 } from "./signin.style";
+import Navbar from "../../non_login_navbar/Navbar";
 
 const SignIn = () => {
+  const url = "http://localhost:8080/users";
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const onChangeEmailId = (e) => setEmailId(e.target.value);
+  const onChangePassword = (e) => setPassword(e.target.value);
+
+  const onClickLogin = (e) => {
+    e.preventDefault();
+    if (emailId !== "" && password !== "") {
+      console.log(`emailId : ${emailId}, password : ${password}`);
+      const user = {
+        emailId: emailId,
+        password: password,
+      };
+      axios
+        .post(`${url}/signIn`, user)
+        .then((response) => {
+          sessionStorage.setItem("logined_user", JSON.stringify(response.data));
+          history.push("/home");
+        })
+        .catch((error) => {
+          alert("아이디 또는 비밀번호를 확인해주세요.");
+          window.location.reload();
+        });
+    } else {
+      alert("입력하지않은 항목이 있습니다.");
+    }
+  };
+
   return (
-    <SignInContainer>
-      <LoginContainer>
-        <SignInHeader>로그인</SignInHeader>
-        <InputGroup>
-          <Row>
-            <Label>아이디</Label>
-            <WarnId id="warnId">*이메일이 올바르지 않습니다.</WarnId>
-          </Row>
-          <InputBorder>
-            <LoginInput
-              type="text"
-              name="email"
-              placeholder="이메일 형식의 아이디를 입력해주세요"
-            />
-          </InputBorder>
-        </InputGroup>
+    <>
+      <Navbar />
+      <div className="content-container">
+        <div className="wrapper">
+          <SignInContainer>
+            <LoginContainer>
+              <SignInHeader>로그인</SignInHeader>
+              <InputGroup>
+                <Row>
+                  <Label>아이디</Label>
+                </Row>
+                <InputBorder>
+                  <LoginInput
+                    type="text"
+                    name="email"
+                    placeholder="이메일 형식의 아이디를 입력해주세요"
+                    onChange={onChangeEmailId}
+                  />
+                </InputBorder>
+              </InputGroup>
 
-        <InputGroup>
-          <Row>
-            <Label>비밀번호</Label>
-            <WarnPassword id="warnPwd">
-              * 비밀번호가 올바르지 않습니다.
-            </WarnPassword>
-          </Row>
-          <InputBorder>
-            <LoginInput name="password" placeholder="비밀번호를 입력해주세요" />
-          </InputBorder>
-        </InputGroup>
+              <InputGroup>
+                <Row>
+                  <Label>비밀번호</Label>
+                </Row>
+                <InputBorder>
+                  <LoginInput
+                    name="password"
+                    placeholder="비밀번호를 입력해주세요"
+                    onChange={onChangePassword}
+                  />
+                </InputBorder>
+              </InputGroup>
 
-        <LoginLinkContainer>
-          <Link to="/home">
-            <OriginLoginBtn>로그인</OriginLoginBtn>
-          </Link>
-        </LoginLinkContainer>
-      </LoginContainer>
-    </SignInContainer>
+              <LoginLinkContainer>
+                <OriginLoginBtn onClick={onClickLogin}>로그인</OriginLoginBtn>
+              </LoginLinkContainer>
+              <Buttons>
+                <Link to="/auth/findPassword">
+                  <ImgBtnContainer>
+                    <OtherLoginBtn>비밀번호 찾기</OtherLoginBtn>
+                  </ImgBtnContainer>
+                </Link>
+              </Buttons>
+            </LoginContainer>
+          </SignInContainer>
+        </div>
+      </div>
+    </>
   );
 };
 
